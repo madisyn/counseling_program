be51221bf49224535bbef43b6ee37d5ee2887074
Madisyn Maldonado <madisyn.hello@hello.com>
2023-03-08 18:18:15 -0800

    Bank commit #2

--- src/Bank.java
+++ src/Bank.java
@@ -4,22 +4,28 @@ public class Bank {
 	static int total;
 	
 	public Bank (int num){
-		this.total = num;
+		Bank.total = num;
 	}
 	
-	public void payEmployees(Employee name) {
-		this.total -= name.salary;
-		name.paid = true;
+	public void payEmployees(Employee name, int month) {
+		Bank.total -= name.MonthlySalary;
+		name.receiveIncome(month);
 	}
 	
-	public void receivePayment(Client name) {
-		this.total += name.paymentAmount;
-		name.paid = true;
+	public void receivePayment(Client name, int month, int week) {
+		//receive money from Client
+		Bank.total += name.paymentAmount;
+		
+		//mark the Client did pay fee
+		name.payExpense(month, week);
 	}
 	
-	public void receivePayment(Intern name) {
-		this.total += name.paymentAmount;
-		name.paid = true;
+	public void receivePayment(Intern name, int month) {
+		//receive money from Intern
+		Bank.total += name.paymentAmount;
+		
+		//mark that the intern did pay fee
+		name.payMonthlyExpense(month);
 	}
 	
 	/*public <T> void receivePayment(T name) {
@@ -27,9 +33,9 @@ public class Bank {
 		name.paid = true;
 	}*/
 	
-	public void voidPayment(Type name) {
-		this.total -= name.paymentAmount;
-		name.paid = false;
+	public void voidInternPayment(Intern name, int month) {
+		Bank.total -= name.paymentAmount;
+		name.payMonthlyExpense(month);
 	}
 
 }

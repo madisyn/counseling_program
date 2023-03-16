629fc5f6e3381b6899ac5fc8539d613f8d5b9179
Madisyn Maldonado <madisyn.hello@hello.com>
2023-03-15 18:16:18 -0700

    initialized paid array

--- src/Employee.java
+++ src/Employee.java
@@ -2,18 +2,36 @@
 public class Employee {
 	
 	String name;
+	String workType;
 	int MonthlySalary;
-	boolean paid[] = new boolean[12];
+	int hoursPerWeek;
+	boolean paid[] = new boolean[11];
 	
 
 	public Employee(String name, int Salary) {
 		this.name = name;
 		this.MonthlySalary = Salary/12;
+		this.workType = "Hourly";
+		this.hoursPerWeek = 0;
+		for(int i=0; i<paid.length; i++) {
+			paid[i] = false;
+		}
+	}
+	
+	public Employee(String name, String workingType, int Salary) {
+		this.name = name;
+		this.MonthlySalary = Salary/12;
+		this.workType = workingType;
+		this.hoursPerWeek = 40;
+		for(int i=0; i<paid.length; i++) {
+			paid[i] = false;
+		}
 	}
 	
 	public void receiveIncome(int month) {
 		//try/catch if the number is a month
 		paid[month] = true;
+		
 	}
 
 }

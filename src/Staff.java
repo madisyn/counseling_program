de053e92bf13073294bdb125a0407e5d6566f4b6
Madisyn Maldonado <madisyn.hello@hello.com>
2023-03-21 15:50:49 -0700

    Added Intern and client list and add function for it.

--- src/Staff.java
+++ src/Staff.java
@@ -1,4 +1,28 @@
-
+import java.util.ArrayList;
 public class Staff {
+	private ArrayList<Intern> internList;
+	private ArrayList<Client> clientList;
+	
+	public Staff() {
+		internList = new ArrayList<Intern>();
+		clientList = new ArrayList<Client>();
+	}
+	
+	public ArrayList<Intern> getInternList(){
+		return internList;
+	}
+	
+	public ArrayList<Client> getClientList(){
+		return clientList;
+	}
+	
+	
+	public void addIntern(Intern name) {
+		internList.add(name);		
+	}
+	
+public void addClient(Client name) {
+		clientList.add(name);
+	}
 
 }

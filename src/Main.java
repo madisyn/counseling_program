21e1f2cd73c94230c350fe1ea4e857421187afe1
Madisyn Maldonado <madisyn.hello@hello.com>
2023-03-23 19:30:03 -0700

    added Pay Employee function and receveFromIntern function

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,59 @@
- 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	/*
+	 * Will check if employee was already paid if not then will check if bank has enough money
+	 * if the bank has enough money then money will be given to employee
+	 */
+	public static void PayEmployee(Employee name, Bank bank, int Month) {
+		
+		if(bank.BankPaymentCheck(name.MonthlySalary) && (name.checkPaymentMonth(Month))){
+			bank.payEmployees(name, Month);
+			System.out.println(name.name + " was paid $" + name.MonthlySalary);
+			System.out.println(name.paid[Month]);
+		}
+		else if(!bank.BankPaymentCheck(name.MonthlySalary)) {
+			System.out.println("Bank does not have enough funds.  Please talk to manager");
+		}
+		else if(name.paid[Month] == true) {
+			System.out.println("Employee was already paid for this month.  Please talk to a mangaer");
+		}
+		
+	}
+	
+	public static void receiveFromIntern(Intern name, Bank bank, int month){
+		if(!name.checkPaymentMonth(month)) {
+			bank.receivePayment(name, month);
+			System.out.println("Intern " + name.name + " payment received");
+		}
+		else {
+			System.out.println("Intern " +name.name +" has already made a payment for this month.  Please select a correct month");
+		}
+	}
+	
+	public void receiveFromClient(){
+		
+	}
+	
+	public void voidClient() {
+		
+	}
+	
+	public void voidIntern() {
+		
+	}
+	
+	public static void printStaffClients(Staff name) {
+		ArrayList<Client> clientList = new ArrayList<>();
+		clientList = name.getClientList();
+		for(int i=0; i<clientList.size(); i++) {
+			System.out.println("check: " + clientList.get(i).name);
+		}
 	}
 
 }
